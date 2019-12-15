530f30e932a60e9bce0927902dcb4e6f4ba42ecb
ZZRRegion <854974242@qq.com>
2019-12-15 11:11:52 +0800

    提交信息

---
+++ IPLocation/GetIpModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPLocation
+{
+    public class GetIpModel
+    {
+        public double longitude { get; set; }
+        public string city { get; set; }
+        public string timezone { get; set; }
+        public int offset { get; set; }
+        public string region { get; set; }
+        public int asn { get; set; }
+        public string organization { get; set; }
+        public string country { get; set; }
+        public string ip { get; set; }
+        public double latitude { get; set; }
+        public string continent_code { get; set; }
+        public string country_code { get; set; }
+        public string region_code { get; set; }
+    }
+}
---
+++ IPLocation/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace IPLocation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            string result = await httpClient.GetStringAsync("https://api.ip.sb/geoip");
+            this.txt.Text = result;
+            System.Runtime.Serialization.Json.DataContractJsonSerializer json = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(GetIpModel));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            GetIpModel model = json.ReadObject(ms) as GetIpModel;
+            this.DataContext = model;
+        }
+    }
+}
